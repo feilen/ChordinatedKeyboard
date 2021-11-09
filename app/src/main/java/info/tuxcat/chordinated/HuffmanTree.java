858402811f3fba4e35412cd7320c218b86276a0f
Chelsea Jaggi <feilen1000@gmail.com>
2021-11-08 21:46:39 -0800

    Add license headers

--- app/src/main/java/info/tuxcat/chordinated/HuffmanTree.java
+++ app/src/main/java/info/tuxcat/chordinated/HuffmanTree.java
@@ -1,3 +1,19 @@
+// Builds a Huffman coding tree from the given nodes, and allows traversal.
+// Copyright (C) 2021 Chelsea Jaggi
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see https://www.gnu.org/licenses/.
+
 package info.tuxcat.chordinated;
 
 import java.util.ArrayList;
